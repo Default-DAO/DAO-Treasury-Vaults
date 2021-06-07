e044c78a1ee706324282c72cd76cd6396be0bdb3
jasonGhostDev <hyotiger777@gmail.com>
2021-06-07 16:19:48 +1000

    usdt vault

--- contracts/USDCVault.sol
+++ contracts/USDCVault.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.8.0;
 
 import "./interfaces/IDNT.sol";
+import "./interfaces/IUSDCShare.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
@@ -8,22 +9,58 @@ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract USDCVault is Ownable, ReentrancyGuard {
     using SafeMath for uint256;
-   
-
-    /* array of address of tokens in the pool */
-    address[] assets;   
+     
     /* the address of the token contract */
-    IDNT private tokenReward; 
+    IDNT private tokenDNT; 
+    IUSDCShare private tokenUSDCShare;
+    IERC20 private tokenUSDC;
 
+    /* address of USDC*/
+    address constant public USDC = address(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
 
-    constructor(IDNT _token) {
-        tokenReward = _token;
-       
+    /* vault fee */
+    uint256 constant vaultFee = 10;
+
+    /* deposited usdc amount of users */
+    mapping (address => uint256) private balances; 
+
+    /* events */
+    event USDCShareMinted(address owner, uint256 amountMinted);
+    event USDCShareBurned(address owner, uint256 amountBurned);
+
+    constructor(IDNT _tokenDNT, IUSDCShare _tokenUSDCShare ) {
+        tokenDNT = _tokenDNT;
+        tokenUSDCShare = _tokenUSDCShare;
+        tokenUSDC = IERC20(USDC);
     }
 
     function deposit_usdc(uint256 amount) public {
         require(amount > 0, "You must send greater than zero");
-   
+        uint256 usdcSharesOutstanding = tokenUSDCShare.totalSupply();
+        uint256 usdcBalance = tokenUSDC.balanceOf(address(this));
+        uint256 mintRate = usdcSharesOutstanding / usdcBalance;
+        uint256 mintAmount = amount * mintRate;
+        tokenUSDC.transferFrom(msg.sender, address(this), amount);
+        balances[msg.sender] += amount;
+      
+        tokenUSDCShare.mint(msg.sender, mintAmount);  
+        emit USDCShareMinted(msg.sender, mintAmount);
+    }
+    function withdraw_usdc(uint256 amount) public {
+        uint256 balance = balances[msg.sender];
+        require(balance > amount, "Withdraw amount exceed");
+        uint256 usdcSharesOutstanding = tokenUSDCShare.totalSupply();
+        uint256 usdcBalance = tokenUSDC.balanceOf(address(this));
+        uint256 burnRate = usdcSharesOutstanding / usdcBalance;
+        uint256 burnAmount = amount * burnRate;
+        uint256 withdrawAmount = amount * (100-vaultFee) / 100;
+        tokenUSDCShare.burn(msg.sender, burnAmount);
+       
+        balances[msg.sender] -= withdrawAmount;
+        tokenUSDC.transfer(msg.sender, withdrawAmount);
+        emit USDCShareBurned(msg.sender, burnAmount);
+     
+
     }
   
 
