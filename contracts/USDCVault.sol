391724e36b9bac1e3315eb8bf209f6910ff7b339
jasonGhostDev <hyotiger777@gmail.com>
2021-06-08 18:42:20 +1000

    add charge_expense

--- contracts/USDCVault.sol
+++ contracts/USDCVault.sol
@@ -28,7 +28,7 @@ contract USDCVault is Ownable, ReentrancyGuard {
     event USDCShareMinted(address owner, uint256 amountMinted);
     event USDCShareBurned(address owner, uint256 amountBurned);
     event changedVaultFee(uint256 newFee);
-
+    event chargedxpense(uint256 amount);
     constructor(IDNT _tokenDNT, IUSDCShare _tokenUSDCShare ) {
         tokenDNT = _tokenDNT;
         tokenUSDCShare = _tokenUSDCShare;
@@ -66,7 +66,10 @@ contract USDCVault is Ownable, ReentrancyGuard {
         emit changedVaultFee(new_fee);
     }
     function charge_expense(uint256 amount) public onlyOwner {
-
+        uint256 usdcBalance = tokenUSDC.balanceOf(address(this));
+        require(usdcBalance > amount, "Withdraw amount exceed");
+        tokenUSDC.transfer(msg.sender, amount);
+        emit chargedxpense(amount);
     }
   
 }
